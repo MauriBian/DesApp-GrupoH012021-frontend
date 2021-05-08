6e2b40335a70059aa2f0b58c6ff11a56d50e6afb
mobianchi <mobianchi@gmail.com>
2021-05-08 19:27:09 -0300

    Example login page

---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login__container">
+    <div class="card card-pad-rg card-login">
+      <h2> Iniciar sesión</h2>
+      <p> {{ contador }} </p>
+      <p> {{ sumarAlContador }} </p>
+      <button @click="sumar" class="btn btn-fit btn-success"> Sumar </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {
+  },
+
+  data () {
+    return {
+      contador: 0
+    }
+  },
+
+   computed: {
+     sumarAlContador () {
+       return this.contador + 1
+     }
+  },
+
+  methods : {
+    sumar () {
+      this.contador += 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.login__container {
+  display:flex;
+  flex-direction: column;
+  .card-login {
+    background-color: $color-primary
+  }
+}
+</style>
